88c869fe55797dcecb746c33257ee4c94731fbce
yaruopooner <yaruopooner@gmail.com>
2015-01-03 05:20:09 +0900

    update sample

--- sample.ps1
+++ sample.ps1
@@ -4,8 +4,9 @@
 #$hoge += '\llvm-builder.ps1'
 $builderShell = Join-Path @(Split-Path $myInvocation.MyCommand.path -Parent) 'llvm-builder.ps1'
 
-$cmake = "c:/cygwin-x86_64/tmp/cmake-2.8.12.2-win32-x86/bin"
+# $cmake = "c:/cygwin-x86_64/tmp/cmake-2.8.12.2-win32-x86/bin"
 # $cmake = "c:/cygwin-x86_64/tmp/cmake-3.0.2-win32-x86/bin"
+$cmake = "c:/cygwin-x86_64/tmp/cmake-3.1.0-win32-x86/bin"
 $python = "c:/Python27"
 $gnu32 = "c:/cygwin-x86_64/tmp/GnuWin32"
 
@@ -13,10 +14,15 @@ $gnu32 = "c:/cygwin-x86_64/tmp/GnuWin32"
 # . $builderShell -targets @("SVN") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
 # . $builderShell -targets @("CMAKE") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
 # . $builderShell -targets @("CMAKE") -clangVersion 350 -platform 32 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
-. $builderShell -targets @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
-. $builderShell -targets @("MSBUILD") -clangVersion 350 -platform 32 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
-# . $builderShell -targets @("CMAKE", "MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
+# . $builderShell -targets @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
+# . $builderShell -targets @("MSBUILD") -clangVersion 350 -platform 32 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
+. $builderShell -targets @("CMAKE", "MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
 # . $builderShell -targets @("CMAKE", "MSBUILD") -clangVersion 350 -platform 32 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32
+# . $builderShell -tasks @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32 -target "Clang libraries\libclang"
+# . $builderShell -tasks @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32 -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Debug" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32 -target "Clang libraries\libclang"
+# . $builderShell -tasks @("MSBUILD") -clangVersion 350 -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -gnu32Path $gnu32 -target "Clang executables\clang-format"
+
 
 # $cmake = "c:/cygwin-x86_64/tmp/cmake-3.0.2-win32-x86/bin"
 
