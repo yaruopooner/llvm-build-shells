d81d457ded92fda766a4d06efd26285af3a2a9dc
yaruopooner <yaruopooner@gmail.com>
2015-01-25 15:22:27 +0000

    modified file permission

--- sh/sample.sh
+++ sh/sample.sh
@@ -5,6 +5,6 @@
 
 # executeRebuild -clangVersion 350 > llvm-build.log 2>&1
 
-executeCheckoutAndBuild -clangVersion 350 > llvm-build.log 2>&1
-# executeCheckoutAndBuild -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "/tmp/ac-clang/clang-server/patch/invalid-mmap.svn-patch"  -patchApplyLocation "llvm/clang/" -patchPath "/tmp/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
+# executeCheckoutAndBuild -clangVersion 350 > llvm-build.log 2>&1
+executeCheckoutAndBuild -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "~/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch"  -patchApplyLocation "llvm/clang/" -patchPath "~/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
 
