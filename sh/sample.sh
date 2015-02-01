d7ad6b8545c6fda515217826ae5def3e0c09655e
yaruopooner <yaruopooner@gmail.com>
2015-02-02 02:10:45 +0900

    update

--- sh/sample.sh
+++ sh/sample.sh
@@ -6,7 +6,7 @@
 # executeRebuild -clangVersion 350 > llvm-build.log 2>&1
 
 # executeBuilder -clangVersion 350 > llvm-build.log 2>&1
-# executeBuilder -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "~/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch"  -patchApplyLocation "llvm/clang/" -patchPath "~/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
-# executeBuilder -checkout -patch -configure -build -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch"  -patchApplyLocation "llvm/tools/clang/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
-executeBuilder -checkout -patch -configure -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch"  -patchApplyLocation "llvm/tools/clang/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
+# executeBuilder -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "~/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch" -patchApplyLocation "llvm/clang/" -patchPath "~/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
+# executeBuilder -checkout -patch -configure -build -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch" -patchApplyLocation "llvm/tools/clang/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
+executeBuilder -checkout -patch -configure -clangVersion 350 -patchApplyLocation "llvm/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalid-mmap.svn-patch" -patchApplyLocation "llvm/tools/clang/" -patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/libclang-x86_64.svn-patch" > llvm-build.log 2>&1
 
