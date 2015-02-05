145ba57ef6e1f7f79dba996d6fecab87eb3273c3
yaruopooner <yaruopooner@gmail.com>
2015-02-05 23:28:55 +0900

    remove old comment

--- sh/sample.sh
+++ sh/sample.sh
@@ -3,7 +3,6 @@
 
 . ./llvm-builder.sh
 
-# executeRebuild --clangVersion 350 > llvm-build.log 2>&1
 
 executeBuilder --checkout --patch --configure --build --clangVersion 350 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
 # executeBuilder --checkout --patch --configure --clangVersion 350 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
