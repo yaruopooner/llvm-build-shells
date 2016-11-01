1deb7fb959c4970c802650f03ed56fd6191c98a8
yaruopooner <yaruopooner@gmail.com>
2016-11-01 20:33:12 +0900

    fixed same local variable names

--- ps1/sample.ps1
+++ ps1/sample.ps1
@@ -7,7 +7,7 @@ $msys2 = ( Join-Path $launchPath "tools-latest-version/msys64/mingw64/bin;" ) + 
 # $gnu32 = "c:/cygwin-x86_64/tmp/llvm-build-shells/ps1/tools-latest-version/GnuWin32/bin"
 
 
-$patchInfos = @( 
+$patches = @( 
     @{ # llvm bug : fixed out of range
         applyLocation = "llvm/tools/clang/";
         absolutePath = ( Resolve-Path ( Join-Path $launchPath "../patch/bugfix000.patch" ) );
@@ -20,42 +20,43 @@ $patchInfos = @(
 
 
 # full task
-# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2015 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
-# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
-# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
-# . $builderShell -tasks @("PROJECT") -clangVersion 390 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
-# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2015 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("PROJECT") -clangVersion 390 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
 
-# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 390 -patchInfos $patchInfos
-. $builderShell -tasks @("PATCH") -clangVersion 390 -patchInfos $patchInfos
-# . $builderShell -tasks @("PROJECT") -clangVersion 390 -msvcVersion 2013 -platform 64 -cmakePath $cmake -msys2Path $msys2
+# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 390 -patchInfos $patches
+# . $builderShell -tasks @("PATCH") -clangVersion 390 -patchInfos $patches
+. $builderShell -tasks @("PROJECT") -clangVersion 390 -msvcVersion 2013 -platform 64 -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("PROJECT") -clangVersion 390 -msvcVersion 2013 -platform 32 -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2013 -platform 32 -configuration "Release" -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Debug" -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "RelWithDebInfo" -target "Clang libraries\libclang"
 
-# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 380 -patchInfos $patchInfos
+# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 380 -patchInfos $patches
+. $builderShell -tasks @("PATCH") -clangVersion 380 -patchInfos $patches
 # . $builderShell -tasks @("PROJECT") -clangVersion 380 -msvcVersion 2013 -platform 64 -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("PROJECT") -clangVersion 380 -msvcVersion 2013 -platform 32 -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("BUILD") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Release" -target "Clang libraries\libclang"
+# . $builderShell -tasks @("BUILD") -clangVersion 380 -msvcVersion 2013 -platform 32 -configuration "Release" -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Debug" -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "RelWithDebInfo" -target "Clang libraries\libclang"
-# . $builderShell -tasks @("BUILD") -clangVersion 380 -msvcVersion 2013 -platform 32 -configuration "Release" -target "Clang libraries\libclang"
 
 
-# . $builderShell -tasks @("PROJECT") -clangVersion 380 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
-# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("PROJECT") -clangVersion 380 -msvcVersion 2013 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
 
-# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang;Clang executables\clang-format"
+# . $builderShell -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -clangVersion 380 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang;Clang executables\clang-format"
 
 
 # libclang only
-# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 390 -patchInfos $patchInfos
-# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang"
-# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang"
-# . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2015 -platform 64 -configuration "Debug" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang"
-# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2015 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos -target "Clang libraries\libclang"
+# . $builderShell -tasks @("CHECKOUT", "PATCH") -clangVersion 390 -patchInfos $patches
+# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang"
+# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2013 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang"
+# . $builderShell -tasks @("BUILD") -clangVersion 390 -msvcVersion 2015 -platform 64 -configuration "Debug" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang"
+# . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -msvcVersion 2015 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches -target "Clang libraries\libclang"
 
 # . $builderShell -tasks @("CHECKOUT", "PROJECT", "BUILD") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2
 
@@ -67,7 +68,7 @@ $patchInfos = @(
 # . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("CHECKOUT", "PROJECT") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2
 # . $builderShell -tasks @("PROJECT") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2
-# . $builderShell -tasks @("PATCH") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patchInfos
+# . $builderShell -tasks @("PATCH") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -patchInfos $patches
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -platform 64 -configuration "Release" -cmakePath $cmake -msys2Path $msys2 -target "Clang libraries\libclang"
 # . $builderShell -tasks @("BUILD") -clangVersion 390 -platform 64 -configuration "Debug" -cmakePath $cmake -msys2Path $msys2 -target "Clang libraries\libclang"
 # . $builderShell -tasks @("PROJECT", "BUILD") -clangVersion 390 -platform 32 -configuration "Release" -cmakePath $cmake -msys2Path $msys2
