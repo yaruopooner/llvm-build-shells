354606f38870f3777083c4ca82a2480513185846
yaruopooner <yaruopooner@gmail.com>
2016-11-07 19:38:22 +0900

    modified accepted clang version from argument

--- sh/sample.sh
+++ sh/sample.sh
@@ -3,21 +3,22 @@
 
 . ./llvm-builder.sh
 
-readonly PROJECT_PATH=$( cd $(dirname ${0}) && cd .. && pwd )
-readonly PATCHS_PATH="${PROJECT_PATH}/patch"
+declare -r CLANG_VERSION="${1-390}"
+declare -r PROJECT_PATH=$( cd $(dirname ${0}) && cd .. && pwd )
+declare -r PATCHS_PATH="${PROJECT_PATH}/patch"
 
 # echo "llvm/tools/clang/;${PATCHS_PATH}/bugfix000.patch"
 
 # executeBuilder --help
 
-# executeBuilder --checkout --patch --configure --build --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" --buildType "Debug" > llvm-build.log 2>&1
-# executeBuilder --checkout --patch --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
-# executeBuilder --checkout --patch --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
-# executeBuilder --checkout --patch --clangVersion 390 --patchInfo "llvm/tools/clang/;${PATCHS_PATH}/bugfix000.patch" --patchInfo "llvm/;/home/test/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
-executeBuilder --checkout --patch --configure --build --clangVersion 390 --patchInfo "llvm/tools/clang/;${PATCHS_PATH}/bugfix000.patch" --patchInfo "llvm/;/home/test/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
-# executeBuilder --checkout --patch --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
-# executeBuilder --configure --build --clangVersion 390 --buildType "Debug" --projectBuilder "cmake" > llvm-build-debug.log 2>&1
-# executeBuilder --configure --build --clangVersion 390 --buildType "Release" --projectBuilder "cmake" > llvm-build-release.log 2>&1
-# executeBuilder --patch --build --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" --buildType "Debug" > llvm-build.log 2>&1
-# executeBuilder --checkout --patch --configure --clangVersion 390 --patchApplyLocation "llvm/" --patchPath "/home/yaruopooner/work/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
+# executeBuilder --checkout --patch --configure --build --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" --buildType "Debug" > llvm-build.log 2>&1
+# executeBuilder --checkout --patch --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
+# executeBuilder --checkout --patch --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
+# executeBuilder --checkout --patch --clangVersion "${CLANG_VERSION}" --patchInfo "llvm/tools/clang/;${PATCHS_PATH}/bugfix000.patch" --patchInfo "llvm/;${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
+executeBuilder --checkout --patch --configure --build --clangVersion "${CLANG_VERSION}" --patchInfo "llvm/tools/clang/;${PATCHS_PATH}/bugfix000.patch" --patchInfo "llvm/;${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
+# executeBuilder --checkout --patch --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build-prepare.log 2>&1
+# executeBuilder --configure --build --clangVersion "${CLANG_VERSION}" --buildType "Debug" --projectBuilder "cmake" > llvm-build-debug.log 2>&1
+# executeBuilder --configure --build --clangVersion "${CLANG_VERSION}" --buildType "Release" --projectBuilder "cmake" > llvm-build-release.log 2>&1
+# executeBuilder --patch --build --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/.emacs.d/.emacs25/packages/user/ac-clang/clang-server/patch/invalidate-mmap.patch" --buildType "Debug" > llvm-build.log 2>&1
+# executeBuilder --checkout --patch --configure --clangVersion "${CLANG_VERSION}" --patchApplyLocation "llvm/" --patchPath "${HOME}/work/ac-clang/clang-server/patch/invalidate-mmap.patch" > llvm-build.log 2>&1
 
