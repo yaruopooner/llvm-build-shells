fffdce868f44c4a1edfab6e1ad3df7775fae58b9
yaruopooner <yaruopooner@mail.com>
2019-10-29 11:15:00 +0900

    RC1 for powershell

--- ps1/sample.ps1
+++ ps1/sample.ps1
@@ -11,24 +11,24 @@ $python = Join-Path $launchPath "tools-latest-version/msys64/mingw64/bin;"
 
 . $builderShell
 
-$llvmCheckoutTag = "llvmorg-8.0.0"
-# $msvcProductName = 2019
-$msvcProductName = 2017
+$llvmCheckoutTag = "llvmorg-9.0.0"
+$msvcProductName = 2019
+# $msvcProductName = 2017
 
 
 # LLVM ALL Build (full task)
-# executeBuilder -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python
+executeBuilder -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -gitPath $git -cmakePath $cmake -pythonPath $python
 
 
 # LLVM Parts Build (full task)
 # $target = "Clang libraries\libclang;Clang executables\clang-format"
 # $target = "Clang libraries\libclang;Clang executables\clang-format;Clang executables\clang-include-fixer;Clang executables\clang-rename;Clang executables\clang-tidy"
-# executeBuilder -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -target $target
+# executeBuilder -tasks @("CHECKOUT", "PATCH", "PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -gitPath $git -cmakePath $cmake -pythonPath $python -target $target
 
 
 # LLVM Parts Build (parts task)
 # $target = "Clang libraries\libclang;Clang executables\clang-format"
-# executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag
+# executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
 # executeBuilder -tasks @("PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python -target $target
 # executeBuilder -tasks @("PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -cmakePath $cmake -pythonPath $python
 # executeBuilder -tasks @("PROJECT", "BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 32 -configuration "Release" -cmakePath $cmake -pythonPath $python
@@ -36,13 +36,12 @@ $msvcProductName = 2017
 
 # LLVM Parts Build (single task)
 # $target = "Clang libraries\libclang;Clang executables\clang-format"
-# executeBuilder -tasks @("CHECKOUT") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git -llvmCheckoutTag $llvmCheckoutTag
-# executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git -llvmCheckoutTag $llvmCheckoutTag
+# executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
 # executeBuilder -tasks @("CHECKOUT") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
-# executeBuilder -tasks @("PATCH") -llvmCheckoutTag $llvmCheckoutTag
-executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
-executeBuilder -tasks @("PROJECT") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -cmakePath $cmake -pythonPath $python
-executeBuilder -tasks @("PROJECT") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 32 -cmakePath $cmake -pythonPath $python
+# executeBuilder -tasks @("PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
+# executeBuilder -tasks @("CHECKOUT", "PATCH") -llvmCheckoutTag $llvmCheckoutTag -gitPath $git
+# executeBuilder -tasks @("PROJECT") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -cmakePath $cmake -pythonPath $python
+# executeBuilder -tasks @("PROJECT") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 32 -cmakePath $cmake -pythonPath $python
 # executeBuilder -tasks @("BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 64 -configuration "Release" -target $target
 # executeBuilder -tasks @("BUILD") -llvmCheckoutTag $llvmCheckoutTag -msvcProductName $msvcProductName -platform 32 -configuration "Release" -target $target
 
